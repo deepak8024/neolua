97d7a0516370ed57d7ee0d6080e3771c6b2d273c
Deepak Kumar <deepakbaba024@gmail.com>
2023-02-03 10:26:33 +0530

    keybinding.lua get modified

--- plugin/toggleterm.lua
+++ plugin/toggleterm.lua
@@ -4,7 +4,7 @@ if not status_ok then
 end
 
 toggleterm.setup({
-    size = 20,
+    size = 100,
     open_mapping = [[<c-\>]],
     hide_numbers = true,
     shade_filetypes = {},
@@ -13,7 +13,7 @@ toggleterm.setup({
     start_in_insert = true,
     insert_mappings = true,
     persist_size = true,
-    direction = "float",
+    direction = "vertical",
     close_on_exit = true,
     shell = vim.o.shell,
     float_opts = {
@@ -26,7 +26,20 @@ toggleterm.setup({
     },
 })
 
+function _G.set_terminal_keymaps()
+  local opts = {buffer = 0}
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+end
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+
 
 
 --vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+
+
 
